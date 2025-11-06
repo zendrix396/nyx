e58b47a176e1bcfc124db9d0e133a59057a5ea72
zendrix396 <yzendric@gmail.com>
2025-11-06 23:58:48 +0530

    fix test

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,46 +3,46 @@ use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut,
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .plugin(
-      tauri_plugin_global_shortcut::Builder::new()
-        .with_handler(move |app, shortcut, event| {
-          if let (ShortcutState::Pressed, Some(window)) =
-            (event.state(), app.get_webview_window("main"))
-          {
-            if shortcut.matches(Modifiers::empty(), Code::F4) {
-              if let Ok(true) = window.is_visible() {
-                let _ = window.hide();
-              } else {
-                let _ = window.show();
-                let _ = window.set_focus();
-              }
+    tauri::Builder::default()
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::new()
+                .with_handler(move |app, shortcut, event| {
+                    if let (ShortcutState::Pressed, Some(window)) =
+                        (event.state(), app.get_webview_window("main"))
+                    {
+                        if shortcut.matches(Modifiers::empty(), Code::F4) {
+                            if let Ok(true) = window.is_visible() {
+                                let _ = window.hide();
+                            } else {
+                                let _ = window.show();
+                                let _ = window.set_focus();
+                            }
+                        }
+                    }
+                })
+                .build(),
+        )
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
             }
-          }
+
+            // Register F4 as the global shortcut
+            app.global_shortcut()
+                .register(Shortcut::new(None, Code::F4))?;
+
+            Ok(())
         })
-        .build(),
-    )
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      
-      // Register F4 as the global shortcut
-      app.global_shortcut()
-        .register(Shortcut::new(None, Code::F4))?;
-      
-      Ok(())
-    })
-    .build(tauri::generate_context!())
-    .expect("error while running tauri application")
-    .run(|_app_handle, event| {
-      // Prevent app from exiting when window is closed
-      if let RunEvent::ExitRequested { api, .. } = event {
-        api.prevent_exit();
-      }
-    });
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|_app_handle, event| {
+            // Prevent app from exiting when window is closed
+            if let RunEvent::ExitRequested { api, .. } = event {
+                api.prevent_exit();
+            }
+        });
 }
